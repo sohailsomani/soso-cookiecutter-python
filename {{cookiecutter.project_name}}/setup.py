6939c8cc3fbff80d423315421dbc5e7b05b8701f
Sohail Somani <me@sohailsomani.com>
2021-01-24 20:39:53 -0500

    Namespace packages

--- {{cookiecutter.project_name}}/setup.py
+++ {{cookiecutter.project_name}}/setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sohailsomani/soso-{{cookiecutter.project_name}}",
-    packages=setuptools.find_packages(include=['soso.*']),
+    packages=['soso.{{cookiecutter.project_name}}'],
     classifiers=[
         "Programming Language :: Python :: 3.8",
         "License :: OSI Approved :: MIT License",
